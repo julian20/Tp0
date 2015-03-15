ae3bbb8266a97c84f6110201015df257253ff991
Julian <juliandambrosio@live.com.ar>
2015-03-15 20:48:45 -0300

    Clase Rock.java

---
+++ 75-10-TP0-rock-paper-scissors/src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Rock.java
@@ -0,0 +1,19 @@
+package main.java.ar.fiuba.tecnicas.rockpaperscissors;
+
+public class Rock {
+
+	public Paper vs(Paper paper)
+	{
+		return paper;
+	}
+	
+	public Rock vs(Rock rock)
+	{
+		return this;
+	}
+	
+	public Rock vs(Scissors scissors)
+	{
+		return this;
+	}
+}
